3c074a8a687ac024df1be70f0fa43abe4c78a33e
Ankit Kanyal <35369589+ANKIT9761@users.noreply.github.com>
2020-06-09 18:28:42 +0530

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,13 @@
+import sys
+
+
+t = int(input().strip())
+
+for a0 in range(t):
+    l=[]
+    n = int(input().strip())
+    for i in range(1,n):
+        if(i%3==0 or i%5==0):
+            l.append(i)
+    print(sum(l))
 
